fca09cac24470414a9fbe2c4a08c8cd296d76018
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-22 16:25:12 +0100

    bonus 1 - composition

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <?php
 
+class Geners {
+  public $generePrincipale;
+  public $genereSecondario;
+
+  public function __construct (string $_generePrincipale, string $_genereSecondario = NULL) {
+    $this -> generePrincipale = $_generePrincipale;
+    $this -> genereSecondario = $_genereSecondario;
+  } 
+}
+
 class Movie {
   public $title;
   public $origin;
   public $year;
+  public $geners;
 
-  public function __construct (string $_title, string $_origin, string $_year) {
+  public function __construct (string $_title, string $_origin, string $_year, Geners $_geners) {
     $this -> upperTitle($_title);
     $this -> origin = $_origin;
     $this -> year = $_year;
+    $this -> geners = $_geners;
   }
 
   public function upperTitle ($_title) {
@@ -17,7 +29,9 @@ class Movie {
 };
 
 
-$avatar = new Movie ("Avatar", "English", "2010");
+$avatar = new Movie ("Avatar", "English", "2010", new Geners ("Fantascienza"));
+$trecento = new Movie ("300", "English", "2007", new Geners ("Fantascienza", "Storico"));
 
-var_dump($avatar);
+
+var_dump($avatar, $trecento);
 ?>
