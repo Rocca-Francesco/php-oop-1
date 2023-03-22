e11280d59a453e9d1daf46c182ef246c7ad1c5ba
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-22 15:51:51 +0100

    first function

--- index.php
+++ index.php
@@ -5,15 +5,19 @@ class Movie {
   public $origin;
   public $year;
 
-  function __construct (string $_title, string $_origin, string $_year) {
-    $this -> title = $_title;
+  public function __construct (string $_title, string $_origin, string $_year) {
+    $this -> upperTitle($_title);
     $this -> origin = $_origin;
     $this -> year = $_year;
   }
-}
+
+  public function upperTitle ($_title) {
+    return $this -> title = strtoupper($_title);
+  }
+};
 
 
 $avatar = new Movie ("Avatar", "English", "2010");
 
-var_dump($avatar)
+var_dump($avatar);
 ?>
